6d5c9e427853fb4b624450d2fafad4750fc0c2d9
Margo Art <margoart.work@gmail.com>
2017-10-28 18:58:04 -0500

    project completed

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
 let menu = {
     _courses: {
-        _appetizers: [],
-        _mains: [],
-        _desserts: [],
-        get appetizers() {
-
-        },
-        set appetizers(appetizerIn) {
-
-        },
-        get mains() {
-
-        },
-        set mains(mainIn) {
-
-        },
-        get desserts() {
-
-        },
-        set desserts(dessertIn) {
-
-        },
+        appetizers: [],
+        mains: [],
+        desserts: [],
     },
+    get appetizers() {
+        },
+    set appetizers(appetizerIn) {
+        },
+    get mains() {
+        },
+    set mains(mainIn) {
+        },
+    get desserts() {
+        },
+    set desserts(dessertIn) {
+        },
+
     get courses() {
         return {
             appetizers: this._courses.appetizers,
             mains: this._courses.mains,
             desserts: this._courses.desserts
         }
-},
+    },
     addDishToCourse(courseName,dishName,dishPrice) {
         let dish= {
             name: dishName,
@@ -39,17 +34,18 @@ let menu = {
     getRandomDishFromCourse(courseName) {
         let dishes = this._courses[courseName];
         let randomIndex = Math.floor(Math.random() * dishes.length);
+        return dishes[randomIndex];
     },
     generateRandomMeal() {
-        let appetizer = getRandomDishFromCourse(this._appetizers);
-        let main = getRandomDishFromCourse(this._mains);
-        let dessert = getRandomDishFromCourse(this._desserts);
+        let appetizer = this.getRandomDishFromCourse('appetizers');
+        let main = this.getRandomDishFromCourse('mains');
+        let dessert = this.getRandomDishFromCourse('desserts');
         let totalPrice = appetizer.price + main.price + dessert.price;
         return `Your meal is ${appetizer.name}, ${main.name}, ... The price is $${totalPrice}.`;
     }
 };
 menu.addDishToCourse('appetizers', 'Caesar Salad', 4.25);
-menu.addDishToCourse('mains', 'Befsroganoff', 6.25);
+menu.addDishToCourse('mains', 'Befstroganoff', 6.25);
 menu.addDishToCourse('desserts', 'Tiramisu', 5.25);
 let meal = menu.generateRandomMeal();
-conole.log(meal);
+console.log(meal);
